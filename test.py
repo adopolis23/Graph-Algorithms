b6200172a3584063cd630dafecefa0bdc1f39c84
adopolis23 <weinhofer@usf.edu>
2023-03-31 19:58:24 -0400

    create_title_dict and create_profession_dict are now working!

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-#Dictionary Creation
 import pandas as pd
 
 class TitleDictionary:
@@ -43,14 +42,12 @@ class TitleDictionary:
 
         #return the Created {nconst:[movie_titles]} dictionary
         return title_dict 
-    
 
 
 
-
-    
 
     def _create_profession_dict(self):
+
         # Dictionary structure:
             # key: nconst
             # value: actors/directors names with tail as '_a' or '_d'
@@ -67,49 +64,63 @@ class TitleDictionary:
         #  'nm7171552': 'Wolfgang Menge_a',
         #  'nm0231693': 'Blake Edwards_d',
         #  'nm6446679': 'Bob Wehling_a'}
+
         profession_dict = {}
 
-        return profession_dict #return the Created {nconst:person_name_a/d} dictionary
+
+        #for each row in the dataframe
+        for index, row in self.df.iterrows():
+
+            suffix = ""
+            if row['primaryProfession'] == "director":
+                suffix = "_d"
+            else:
+                suffix = "_a"
+
+            #if this person is not in the dict already
+            if profession_dict.get(row['nconst']) == None:
+
+                #add their name to the dict plus the suffix
+                profession_dict[row['nconst']] = row['primaryName'] + suffix
+            
+            #if this person is in the dict already -Dont think this path is taken
+            else:
+                continue
+
+        
+        print(profession_dict['nm0465106'])
+
+        #return the Created {nconst:person_name_a/d} dictionary
+        return profession_dict
 
 
-#Graph Network Creation
-class MovieNetwork:
-    def __init__(self, name_movie_dict, nconst_ar_dr):
-        self.graph = {} #graph dictionary initialization
-        self.name_movie_dict = name_movie_dict #name_movie_dict is nothing but "title_dict" dictionary refer to above example in TitleDictionary.
-        self.nconst_ar_dr = nconst_ar_dr #it is "profession_dict" dictionary refer to above example in TitleDictionary.
 
 
-    def add_node(self, node):
-        #write code to add node to the graph (dictionary data-structure)
+td = TitleDictionary('imdb_network.csv')
 
-    def add_edge(self, node1, node2, nconst_ar_dr, weight=1):
-        #node 1, node 2: nconst id's
-        #nconst_ar_dr is nothing but "profession_dict" dictionary refer to above example in TitleDictionary.
-        #weight is number of common movie titles exists in node1 and node2
-        #Before adding Edge weights you must follow the below Instructions:
-            #1. consider only the node1->node2 connection or edge, only if node1 and node2 have more than 2 movies in common.
-            #2. Let node1="actor" and node2="director" then node1->node2 edge should not be taken implies {actor:{director:6}} must not be taken.
-                # But node2->node1 should be taken implies {director:{actor:6}} must be taken.
-            #3. if node1 and node2 are assigned with both actors or directors then bi-directional edge must be added implies
-                #{actor1:{actor2:4}} and {actor2:{actor1:4}} or {director1:{director2:7}} and {director2:{director1:7}} both ways are true
-                #and must consider in dictionary.
-        #write code to add edge to the graph implies add weight between node1 and node 2
-        #Example weight assignment looks like:
-        # {'nm1172995': {'nm0962553': 7}} here the weight 7 is nothing but the number of common
-        #movies between two persons either actor/director (nm1172995 and nm0962553)
 
-    def create_graph(self):
-        #By following the above conditions create a graph (use only dictionary datastructure: self.graph)
-        #example graph looks like:
-          # {'nm0962553': {'nm8630849': 3,
-          #     'nm1172995': 7,
-          #     'nm8742830': 16,
-          #     'nm6225202': 4,
-          #     'nm4366294': 4},
-          #    'nm8630849': {},
-          #    'nm1172995': {'nm0962553': 7},
-          #    'nm8742830': {'nm0962553': 16},
-          #    'nm6225202': {}}
 
-        return graph
+
+
+
+
+
+'''
+dict = {}
+
+
+nconst = ['1', '2', '3', '4', '1', '2']
+
+for x in nconst:
+
+    if dict.get(x) == None:
+        print("Not in dict")
+        dict[x] = [0]
+    else:
+        print("In dict")
+        dict[x].append(0)
+
+
+print(dict.get('1'))
+'''
+
